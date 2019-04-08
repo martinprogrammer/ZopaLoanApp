63fa28093e50fc593f380828cbc6942e2364cf6d
martinprogrammer <martinprogrammer@outlook.com>
2019-04-08 18:50:35 +0100

    ZopaService added

---
+++ ZopaService/Models/LoanResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZopaService.Models
+{
+    public class LoanResponse
+    {
+        public string Lender { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Interest { get; set; }
+    }
+}
