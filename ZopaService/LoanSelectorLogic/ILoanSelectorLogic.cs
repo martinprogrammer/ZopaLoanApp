7f7c65eb87d958a03751cc73ea344e7e2c19977e
martinprogrammer <martinprogrammer@outlook.com>
2019-04-09 23:54:48 +0100

    Finished!

--- ZopaService/LoanSelectorLogic/ILoanSelectorLogic.cs
+++ ZopaService/LoanSelectorLogic/ILoanSelectorLogic.cs
@@ -11,5 +11,8 @@ namespace ZopaService.LoanSelectorLogic
     public interface ILoanSelectorLogic
     {
         Message<LoanResponse> GetOptimalLoan(IList<Loan> loans, Message<LoanRequest> request);
+        double CalculateInterest(IList<Loan> loans, decimal amount);
+        double CalculateTotalRepayment(double amount, double interest, double years, double compoundPeriods);
+
     }
 }
---
+++ ZopaServiceTests/ThreeYearCompoundInterestLoanSelectorTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ZopaService.LoanSelectorLogic;
+using ZopaDAL.Models;
+
+namespace ZopaServiceTests
+{
+    [TestFixture]
+    class ThreeYearCompoundInterestLoanSelectorTests
+    {
+        [Test]
+        public void Given_Amount_And_ListOfLoans_CalculateInterest_Should_Return_Correct_Interest()
+        {
+            //arrange
+            ILoanSelectorLogic logic = new ThreeYearCompoundInterestLoanSelector();
+            IList<Loan> loans = new List<Loan>();
+            loans.Add(new Loan(){
+               Lender="Jane", Amount=480, Interest=0.069M
+            });
+            loans.Add(new Loan(){
+               Lender="Fred", Amount=520, Interest=0.071M
+            });
+
+            decimal amount = 1000;
+
+            //act 
+            var result = logic.CalculateInterest(loans, amount);
+
+            //assert
+            Assert.That(result == 7);
+        }
+
+        [Test]
+        public void Given_Amount_Interest_Years_CompoundPeriods_CalculateTotalRepayment_Should_Return_Correct_TotalRepayment()
+        {
+            //arrange
+            ILoanSelectorLogic logic = new ThreeYearCompoundInterestLoanSelector();
+            double amount = 1000;
+            double interest = 7;
+            double years = 3;
+            double compoundPeriods = 12;
+
+            //act
+            var result = logic.CalculateTotalRepayment(amount, interest, years, compoundPeriods);
+
+            //assert
+            Assert.That(result == 1232.9255874769281);
+
+        }
+    }
+}
